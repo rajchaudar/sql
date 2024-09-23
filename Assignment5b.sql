240caee760aca07b9af2b0ad0887df963f736f7d
rajchaudar <rajchaudar60@gmail.com>
2024-09-23 21:29:18 +0530

    Refactor SQL script for library management

---
+++ Assignment5b.sql
@@ -0,0 +1,58 @@
+Create Database Assignment5b;
+
+use Assignment5b;
+
+Create table Library (id INT PRIMARY KEY ,
+Bookname Varchar(50),Publisher Varchar(50),
+Section Varchar(50));
+
+INSERT INTO Library (id, bookname, publisher, section)
+VALUES(1,'TOC','Technical','Engg'),
+(2,'CNS','Techknowledge','Engg'),
+(3,'The Alchemist','HarperCollins','Fiction'),
+(4,'The Da Vinci Code','Doubleday','Thriller'),
+(5,'The Kite Runner','Riverhead','Drama'),
+(6,'The Great Gatsby','Scribner','Literature');
+
+CREATE TABLE LibraryAudit (id INT ,Pre_book_name VARCHAR(50),
+Pre_Publisher VARCHAR(50),Pre_Section VARCHAR(50),Status VARCHAR(50));
+
+DELIMITER //
+
+CREATE TRIGGER Before_update
+BEFORE UPDATE ON Library
+FOR EACH ROW
+BEGIN
+    INSERT INTO LibraryAudit(id, Pre_book_name, Pre_Publisher, Pre_Section,Status)
+    VALUES (OLD.id, OLD.bookname, OLD.publisher, OLD.section,'Before Update');
+END //
+
+CREATE TRIGGER After_update
+AFTER UPDATE ON Library
+FOR EACH ROW
+BEGIN
+    INSERT INTO LibraryAudit(id, Pre_book_name, Pre_Publisher, Pre_Section,Status)
+    VALUES (NEW.id, NEW.bookname, NEW.publisher, NEW.section,'After Update');
+END //
+
+CREATE TRIGGER before_delete
+BEFORE DELETE ON Library
+FOR EACH ROW
+BEGIN
+    INSERT INTO LibraryAudit(id, Pre_book_name, Pre_Publisher, Pre_Section,Status)
+    VALUES (OLD.id, OLD.bookname, OLD.publisher, OLD.section,'Deleted');
+END //
+
+DELIMITER ;
+
+UPDATE Library SET section = 'Literature' WHERE id = 4;
+
+select * from LibraryAudit;
+
+delete from Library where(id = 5);
+
+select * from LibraryAudit;
+
+select * from Library;
+
+
